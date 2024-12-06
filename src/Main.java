72ea20f4bc4de27afc9e1cf9aa5cd519e5cf4eb6
jaeseok <wotjrdl98@naver.com>
2024-12-06 17:52:35 +0900

    dd

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
+        int answer = 0;
+        String a = "hello";
+        String b = "ohell";
+        String str = "";
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < 9;i++){
-            arr[i] = Integer.parseInt(br.readLine());
-
-        }
 
     }
 }
--- src/Practice.java
+++ src/Practice.java
@@ -2,6 +2,11 @@ public class Practice {
     public static void main(String[] args) {
         char a ='a';
         System.out.println();
-    }
 
+        int num1 = 5;
+        int num2 = 2;
+
+        int answer = Integer.parseInt((double)num1/num2);
+        System.out.println(answer);
+    }
 }
