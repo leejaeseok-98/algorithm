efc6c3aa7f7b1a0988c11422dc01a084ba9a2475
jaeseok <wotjrdl98@naver.com>
2024-12-10 13:27:45 +0900

    ㅇㅇ

---
+++ src/baekjoon/Palin.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Palin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        StringBuilder sb = new StringBuilder();
+    }
+}
